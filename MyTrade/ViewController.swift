20ee44d3f025e4b5b6bcbcfa90379224c82e5e62
Arsalan <arakhtar@icloud.com>
2021-04-07 20:18:41 +0800

    Init

---
+++ MyTrade/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTrade
+//
+//  Created by Arsalan Akhtar on 4/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
